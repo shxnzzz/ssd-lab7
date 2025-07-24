4f7970b8b15d2bec10b64307f3157c74a5d7f7cd
shxnzzz <shanjinkok@gmail.com>
2025-07-24 20:21:48 +0800

    lab 7 : app + tests + workflows

--- tests/SeleniumTest.mjs
+++ tests/SeleniumTest.mjs
@@ -6,14 +6,14 @@ const environment = process.argv[2] || 'local';
 
 // URLs based on environment
 // Obtain dev selenium server IP using: docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' selenium-server
-const seleniumUrl = environment === 'github' 
-  ? 'http://selenium:4444/wd/hub' 
-  : 'http://localhost:4444/wd/hub';
+const seleniumUrl = environment === 'github'
+  ? 'http://selenium:4444/wd/hub'
+  : 'http://127.0.0.1:4444/wd/hub';
 
 // Note: Start the nodejs server before running the test locally
 const serverUrl = environment === 'github' 
   ? 'http://testserver:3000' 
-  : 'http://localhost:3000';
+  : 'http://host.docker.internal:3000';
 
 console.log(`Running tests in '${environment}' environment`);
 console.log(`Selenium URL: ${seleniumUrl}`);
